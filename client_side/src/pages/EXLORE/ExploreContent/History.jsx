6e6e33e4faa67ffddc90ea7abcd7b1677092c3c9
Artistoo <99140028+Artistoo@users.noreply.github.com>
2023-04-28 04:07:43 +0100

    push the remining files

--- client_side/src/pages/EXLORE/ExploreContent/History.jsx
+++ client_side/src/pages/EXLORE/ExploreContent/History.jsx
@@ -1,28 +1,3 @@
-import React from "react";
-
-export default function History() {
-  const calcAge = () => {
-    let yearsMonths = [];
-    let suffix;
-    let age = +new Date().getFullYear().toString().slice(2, 4) - 23;
-    suffix = +age ? `year${age > 1 ? "s" : ""}` : `month${age > 1 ? "s" : ""}`;
-    for (let i = age; i <= suffix.includes("month") ? 12 : age + 10; i++) {
-      yearsMonths.push(i);
-    }
-    return yearsMonths;
-  };
-  console.log(calcAge());
-  return (
-    <div
-      style={{
-        background: "#ef8da7",
-      }}
-      className={`w-full ml-[25px] flex flex-col `}
-    >
-      <div className={`flex `}>
-        <h2 id={"historyTitle"}>History of Xillas</h2>
-      </div>
-      <div id={"ageAnimationBox"} className={` h-[50px] aspect-square`}></div>
-    </div>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0851e589f164858c9c65a2d3745067b4ec89ac0e8b908a2f4cd053728cc9d0d2
+size 859
