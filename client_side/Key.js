9a226419b309617a880e9f4f49f90613876b8a44
Artistoo <99140028+Artistoo@users.noreply.github.com>
2023-04-28 03:08:20 +0100

    80% done

--- client_side/Key.js
+++
@@ -1,3 +0,0 @@
-export const keys = {
-    pixaby : `33028013-2a3957987fdc1d3e1f76e61be`
-} 
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,17 +1,3 @@
-//0xa1EE5362c62173f6a1cC183a6706D94Aca54b129
-module.exports = {
-  solidity: "0.8.19",
-  compilers: {
-    solc: {
-      version: "0.8.19",
-    },
-  },
-  networks: {
-    goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/ZCYj16PkzpC8esaD6PtRLXvOQCSZJGfK",
-      accounts: [
-        "6e85e14a0212d08f42921d6124bafc59fbb38ad95443ef2c7903387f8c9f3b42",
-      ],
-    },
-  },
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:f031f3326b45fd427ba4bd82fca059951448c0dab7f4707d22f16c1e082f5bed
+size 335
